0c4ad6dc61e320b6fd9ad42ed9479ffdccd8c0ae
VECTORPRO <Vector59rus@gmail.com>
2022-01-23 19:34:17 +0300

    Добавьте файлы проекта.

---
+++ SB15.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int main()
+{
+	const int N = 20; 
+	int i = 0;
+	
+	// Блок 1 поиск всех четных цифр
+	std::cout << "All even numbers up to: " << N << "\n";
+	while (i <= N)
+	{
+		i = i++;
+		if (i % 2 == 0)
+		std::cout << i << ", ";
+	}
+	std::cout << "\n";
+
+	// Блок 1 поиск всех нечетных цифр
+	std::cout << "All Odd numbers up to: " << N << "\n";
+	i = 0;
+	while (i <= N)
+	{
+		if (i % 2 > 0)
+		std::cout << i << ", ";
+		i = i++;
+	}
+	std::cout << "\n";
+
+
+}
+
+
