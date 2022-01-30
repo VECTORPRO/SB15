2bc60a61927adc070d4bf8a32697687f224e3b1d
VECTORPRO <Vector59rus@gmail.com>
2022-01-30 16:23:41 +0300

    Вернул всё в один файл SB15.cpp но почему то не работает вывод данных на экран функции 2

--- SB15.cpp
+++ SB15.cpp
@@ -1,15 +1,63 @@
 ﻿#include <iostream>
 #include "Numbers.h"
 
+int N = 0;
+int i = 1;
+bool even = true;
+
+int NumberVvod(int N)
+{
+	return N;
+};
+
+void Numbers(int N, bool even)
+{
+
+};
+
 int main()
 {
-	int NumberVvod();
+	int NumberVvod(N);
+	{
+		// Функция 0 Запрос на ввод числа
+		std::cout << "Enter a number: " << "\n";
+		std::cin >> N;
+		return N;
+	}
 
-	void Numbers1();
+		std::cout << "Function 2... " << "\n";
 
-	void Numbers2();
+	void Numbers(int N, bool even);
+		{
+			if (N % 2 > 0)
+			{
+				// Функция поиск всех нечетных цифр или четных цифр
+				std::cout << "All Odd numbers up to: " << "\n";
+				i = 1;
+				while (i <= N)
+				{
+					if (i % 2 > 0)
+						std::cout << i << ", ";
+					i = i++;
+				}
 
-	std::cout << "\n";
+			}
+			else
+			{
+				// Функция 2 поиск всех четных цифр
+				std::cout << "All even numbers up to: " << "\n";
+				while (i <= N)
+				{
+					if (i % 2 == 0)
+						std::cout << i << ", ";
+					i = i++;
+				}
+
+
+			}
+			
+		}
+		std::cout << "\n";
 	
 }
 
